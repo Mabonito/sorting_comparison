05e2193a142d3f0fa3407bcee14abc13ac514276
Mabonito <tbmmotsosi@gmail.com>
2022-03-15 00:08:05 +0200

    Firt Commit

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module sorting_comparison {
+}
---
+++ src/sorting_comparison/lena_sort.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sorting_comparison;
+
+/**
+ * @author tshepang
+ *
+ */
+public class lena_sort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
